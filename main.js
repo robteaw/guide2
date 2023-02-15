4275b642fd5e2cc47612a449db62b67e2d0e1e0a
Robert Tea <robteaw@gmail.com>
2023-02-14 23:23:04 -0600

    14. Onclick and Page Updates

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// Navbar
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
 });
+
+// JavaScript Page
+const codeImgs = ["images/html-5.png", "images/css-3.png", "images/js.png"];
+
+function Random() {
+  let num = Math.floor(Math.random() * 3);
+  document.getElementById("img_click").innerHTML =
+    '<img src=" + codeImgs[num]">';
+}
