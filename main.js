a44f9f0f36316db27a8a8280e7dbd9b77c710b7f
Robert Tea <robteaw@gmail.com>
2023-03-08 23:56:12 -0600

    17. Map Info JS

--- main.js
+++ main.js
@@ -6,14 +6,45 @@ hamburger.addEventListener("click", function () {
 });
 
 // JavaScript Page
+// Onclick Images
 function imgChange(file) {
   let img = document.querySelector("#img_click");
   img.setAttribute("src", file);
 }
 
+// Random Images
 const imgArray = ["./images/heads.png", "./images/tails.png"];
 
 function randomImg() {
   let num = Math.floor(Math.random() * imgArray.length);
   document.getElementById("img_random").src = imgArray[num];
 }
+
+// Fetch
+async function loadUsers() {
+  const response = await fetch("data/users.json");
+  const users = await response.json();
+
+  return users;
+
+  console.log(response);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let users = [];
+  try {
+    users = await loadUsers();
+  } catch (e) {
+    console.log("Error");
+    console.log(e);
+  }
+  console.log(users);
+});
+
+// Map
+const people = [];
+
+const names = people.map((person) => {
+  return person.name;
+});
+console.log(names);
