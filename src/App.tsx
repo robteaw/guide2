b99592fed91af67ccef8636e67f980ad5be77e8c
Robert Tea <robteaw@gmail.com>
2023-03-19 23:44:07 -0500

    24. Setting Up Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, // Switch has been replaced with Routes
+  Route,
+} from "react-router-dom"; // npm i react-router-dom
+import Home from "./pages/Home/Home";
+import Css from './pages/Css/Css';
+import Javascript from './pages/Javascript/Javascript';
+import React1 from './pages/React/React1';
+import Git from './pages/GIt/Git';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/css" element={<Css />} />
+        <Route path="/javascript" element={<Javascript />} />
+        <Route path="/react" element={<React1 />} />
+        <Route path="/git" element={<Git />} />
+        
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
