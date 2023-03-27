a3aee9af9546c7f4e86e70777024667f6304e875
Robert Tea <robteaw@gmail.com>
2023-03-27 00:01:39 -0500

    27. Css Page Update

--- src/pages/Css/Css.tsx
+++ src/pages/Css/Css.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import css from "../../images/code_css.png";
 
 export default function Css() {
   return (
-    <div>Css</div>
+    <>
+    <div className="info">
+        <h1>CSS</h1>
+        <p>Selector - selects all HTML elements</p>
+        <ul>
+          <li>class, id, div, p</li>
+        </ul>
+
+        <h2>Setting up CSS</h2>
+        <img src={css} alt="" />
+    </div>
+
+    <script src="main.js"></script>
+    </>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -78,6 +78,7 @@ export default function Home() {
 
       <Center />
       
+      <script src="main.js"></script>
       </div>
     </>
   )
