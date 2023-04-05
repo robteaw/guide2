700e1211add753139cea5d76fca8895ddbdb7de8
Robert Tea <robteaw@gmail.com>
2023-04-04 23:21:17 -0500

    33. Netlify Setup

--- src/pages/Home/Form.tsx
+++ src/pages/Home/Form.tsx
@@ -52,7 +52,7 @@ export default function Form() {
         </div>
         <div>
           <input
-            type="text"
+            type="tel"
             name="phone"
             id="phone"
             placeholder="Phone"
