aa1fc6cefe0e2583c5ccf44e9ef1ea272ee89d71
Robert Tea <robteaw@gmail.com>
2023-07-18 23:33:44 -0500

    36. Loops Info Added

--- src/pages/Javascript/Javascript.tsx
+++ src/pages/Javascript/Javascript.tsx
@@ -1,4 +1,8 @@
 // images
+import loop from "../../images/loop.png";
+import nest from "../../images/nest.png";
+import while_loop from "../../images/while.png";
+import do_while from "../../images/do_while.png";
 import html5 from "../../images/html-5.png";
 import heads from "../../images/heads.png";
 import filter from "../../images/code_filter.png";
@@ -34,6 +38,29 @@ export default function Javascript() {
     <h2>Value vs Reference</h2>
     <p>value - number, boolean, string, undefined, null</p>
     <p>reference - object, function, array</p>
+
+    <h2>Loops</h2>
+      <h3>For Loop</h3>
+      <p>A for loop contains three expressions separated by ; inside the parentheses:</p>
+      <ol>
+      <li>an initialization starts the loop and can also be used to declare the iterator variable.</li>
+      <li>a stopping condition is the condition that the iterator variable is evaluated against— if the condition evaluates to true the code block will run, and if it evaluates to false the code will stop.</li>
+      <li>an iteration statement is used to update the iterator variable on each loop.
+      </li>
+      </ol>
+      <img className="loopImg" src={loop} alt="" />
+
+      <h3>Nested Loop</h3>
+      <p>When we have a loop running inside another loop.</p>
+      <img className="loopImg2" src={nest} alt="" />
+
+      <h3>While Loop</h3>
+      <p>When we don't know in advance how many times the loop should run.</p>
+      <img className="loopImg3" src={while_loop} alt="" />
+
+      <h3>Do While Loop</h3>
+      <p>Do a task once and then keep doing it until a specified condition is no longer met.</p>
+      <img className="loopImg4" src={do_while} alt="" />
 
     <h2>Onclick</h2>
       <p>Change on individual button click</p>
