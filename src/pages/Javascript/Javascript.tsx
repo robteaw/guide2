62815ca327a377155e4b94ca578034373d99fc51
robteaw <robteaw@gmail.com>
2023-07-20 00:21:18 -0500

    40. Images Added

--- src/pages/Javascript/Javascript.tsx
+++ src/pages/Javascript/Javascript.tsx
@@ -1,4 +1,9 @@
 // images
+import func from "../../images/function.png";
+import func2 from "../../images/function2.png";
+import parameter from "../../images/parameter.png";
+import argument from "../../images/argument.png";
+import arrow from "../../images/arrow.png";
 import loop from "../../images/loop.png";
 import nest from "../../images/nest.png";
 import while_loop from "../../images/while.png";
@@ -46,46 +51,74 @@ export default function Javascript() {
         <p>value - number, boolean, string, undefined, null</p>
         <p>reference - object, function, array</p>
 
-        <h2>Function</h2>
-        <p></p>
+        <div className="function-info">
+          <h2>Functions</h2>
+          <h3>Function Declaration</h3>
+          <p>
+            Declared functions are not executed immediately. They are "saved for
+            later use", and will be executed later, when they are invoked
+            (called upon).
+          </p>
+          <img src={func} alt="" />
 
-        <h2>Loops</h2>
-        <h3>For Loop</h3>
-        <p>
-          A for loop contains three expressions separated by ; inside the
-          parentheses:
-        </p>
-        <ol>
-          <li>
-            an initialization starts the loop and can also be used to declare
-            the iterator variable.
-          </li>
-          <li>
-            a stopping condition is the condition that the iterator variable is
-            evaluated against— if the condition evaluates to true the code block
-            will run, and if it evaluates to false the code will stop.
-          </li>
-          <li>
-            an iteration statement is used to update the iterator variable on
-            each loop.
-          </li>
-        </ol>
-        <img className="loopImg" src={loop} alt="" />
+          <h3>Function Expression</h3>
+          <p>
+            Function expression are stored in a variable and the variable can be
+            used as a function.
+          </p>
+          <img src={func2} alt="" />
 
-        <h3>Nested Loop</h3>
-        <p>When we have a loop running inside another loop.</p>
-        <img className="loopImg2" src={nest} alt="" />
+          <h3>Arrow Function</h3>
+          <img src={arrow} alt="" />
 
-        <h3>While Loop</h3>
-        <p>When we don't know in advance how many times the loop should run.</p>
-        <img className="loopImg3" src={while_loop} alt="" />
+          <h3>Parameter</h3>
+          <img src={parameter} alt="" />
 
-        <h3>Do While Loop</h3>
-        <p>
-          Do a task once and then keep doing it until a specified condition is
-          no longer met.
-        </p>
-        <img className="loopImg4" src={do_while} alt="" />
+          <h3>Argument</h3>
+          <img src={argument} alt="" />
+        </div>
+
+        <div className="loop-info">
+          <h2>Loops</h2>
+          <h3>For Loop</h3>
+          <p>
+            A for loop contains three expressions separated by ; inside the
+            parentheses:
+          </p>
+          <ol>
+            <li>
+              an initialization starts the loop and can also be used to declare
+              the iterator variable.
+            </li>
+            <li>
+              a stopping condition is the condition that the iterator variable
+              is evaluated against— if the condition evaluates to true the code
+              block will run, and if it evaluates to false the code will stop.
+            </li>
+            <li>
+              an iteration statement is used to update the iterator variable on
+              each loop.
+            </li>
+          </ol>
+          <img src={loop} alt="" />
+
+          <h3>Nested Loop</h3>
+          <p>When we have a loop running inside another loop.</p>
+          <img src={nest} alt="" />
+
+          <h3>While Loop</h3>
+          <p>
+            When we don't know in advance how many times the loop should run.
+          </p>
+          <img src={while_loop} alt="" />
+
+          <h3>Do While Loop</h3>
+          <p>
+            Do a task once and then keep doing it until a specified condition is
+            no longer met.
+          </p>
+          <img className="loopImg4" src={do_while} alt="" />
+        </div>
 
         <h2>Onclick</h2>
         <p>Change on individual button click</p>
