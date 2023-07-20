dcb0566673b67e3bfd57fcf27595779d22ebab34
Robert Tea <robteaw@gmail.com>
2023-07-19 21:00:42 -0500

    37. Box Model Info Added

--- src/pages/Css/Css.tsx
+++ src/pages/Css/Css.tsx
@@ -1,3 +1,4 @@
+import box from "../../images/box_model.png";
 import css from "../../images/code_css.png";
 
 export default function Css() {
@@ -11,7 +12,19 @@ export default function Css() {
         </ul>
 
         <h2>Setting up CSS</h2>
-        <img src={css} alt="" />
+        <h3>Box Model</h3>
+        <p>The box model is a box that wraps around each HTML elment. Starting from outer layer to inner, it includes:</p>
+        <ul>
+          <li>Margin</li>
+          <li>Border</li>
+          <li>Padding</li>
+          <li>Content</li>
+        </ul>
+        <div className="box-info">
+          <img src={box} alt="box" />
+          <h3>Box Model in stylesheet</h3>
+          <img src={css} alt="css" />
+        </div>
     </div>
 
     <script src="main.js"></script>
