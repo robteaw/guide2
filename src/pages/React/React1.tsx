23c8ec6ebb93c18476388d207cea979b6c1922fc
Robert Tea <robteaw@gmail.com>
2023-10-07 23:44:03 -0500

    React Page Updated

--- src/pages/React/React1.tsx
+++ src/pages/React/React1.tsx
@@ -11,6 +11,26 @@ export default function React1() {
         <p>cd my-app</p>
         <p>npm start</p>
         <p>npm i</p>
+
+        <h2>Hooks</h2>
+
+
+        <h2>State</h2>
+
+
+        <h2>Context API</h2>
+
+
+        <h1>Typscript</h1>
+        <p>subset of JavaScript</p>
+        
+        <h3>Explicit Type</h3>
+        <p>writing out the type</p>
+        <p>let firstName: string = "Dylan";</p>
+
+        <h3>Implicit Type</h3>
+        <p>will guess the type</p>
+        <p>let firstName = "Dylan";</p>
     </div>
 
     <script src="main.js"></script>
