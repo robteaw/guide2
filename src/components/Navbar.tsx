5fae50429ec6dece6f0880790866e134b0b4dbc6
Robert Tea <robteaw@gmail.com>
2023-10-14 22:21:24 -0500

    Navbar Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,35 @@
+import {useState} from 'react';
 import { Link } from "react-router-dom";
 // imaages
 import logo from "../images/logo.png";
+// icons
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+  const [click, setClick] = useState(false);
+
+   // Close nav menu
+   const handleClick = () => setClick(!click);
+   const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav>
         <div className="container">
-            <Link to="/"><img src={logo} alt="" className="logo" /></Link>
-            <div className="menu">
-                <Link to="/" className="is-active">HTML</Link>
-                <Link to="/css">CSS</Link>
-                <Link to="/javascript">JavaScript</Link>
-                <Link to="/react">React</Link>
-                <Link to="/git">Github</Link>
-                <Link to="/terminal">Terminal</Link>
+            <Link to="/"><img src={logo} alt="" className="logo" onClick={closeMobileMenu} /></Link>
+            <div className={click ? "menu active" : "menu"}>
+                <Link to="/" className="nav-item">HTML</Link>
+                <Link to="/css" className="nav-item">CSS</Link>
+                <Link to="/javascript" className="nav-item">JavaScript</Link>
+                <Link to="/react" className="nav-item">React</Link>
+                <Link to="/git" className="nav-item">Github</Link>
+                <Link to="/terminal" className="nav-item">Terminal</Link>
             </div>
-            {/* <img src={sun} alt="sun" id="bgChange" /> */}
-            {/* onClick={bgChange}  */}
-
-          <button className="hamburger is-active">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
+            <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>        
+          </div>
       </nav>
     </>
-  );
+  )
 }
