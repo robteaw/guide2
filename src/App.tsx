a10c47fef5300d8cd8b9311325bb410e7c08383e
Robert Tea <robteaw@gmail.com>
2023-10-22 10:18:51 -0500

    ScrollToTop component added and margin spacing adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 import {
   BrowserRouter as Router,
   Routes, // Switch has been replaced with Routes
   Route,
 } from "react-router-dom"; // npm i react-router-dom
 import Home from "./pages/Home/Home";
-import Css from './pages/Css/Css';
-import Javascript from './pages/Javascript/Javascript';
-import React1 from './pages/React/React1';
-import Git from './pages/GIt/Git';
-import Terminal from './pages/Terminal/Terminal';
-import Error from './pages/Error';
-import Navbar from './components/Navbar';
+import Css from "./pages/Css/Css";
+import Javascript from "./pages/Javascript/Javascript";
+import React1 from "./pages/React/React1";
+import Git from "./pages/GIt/Git";
+import Terminal from "./pages/Terminal/Terminal";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop"; // Page stays on top when link clicked
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
